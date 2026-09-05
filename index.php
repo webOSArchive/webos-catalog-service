cc99d5dfc2145c109eb757150a7aa21b23843a39
codepoet80 <codepoet80@github.com>
2026-09-05 11:13:23 -0400

    change link

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ echo file_get_contents("https://www.webosarchive.org/menu.php?docRoot=" . $docRo
            | <a class="download-link" href="https://weboslives.eu/feeds/">Alternate Site</a>
            | <a class="download-link" href="https://archive.org/details/webosappcatalog"> Full Archive</a>
         <br><br>
-        <div style="font-weight:bold; margin-bottom:8px;">Install on Device | <a href="http://www.webosarchive.org/docs/appstores/">Help</a><br></div>
+        <div style="font-weight:bold; margin-bottom:8px;">Install on Device | <a href="http://docs.webosarchive.org/#step-5">Help</a><br></div>
         <a class="download-link" href="AppPackages/com.palm.app.enyo-findapps_6.0.2900_all.ipk"><img src="assets/hp-appcatalog.png" style="vertical-align:middle;height:48px;width:48px;" alt="HP App Catalog for TouchPad"" title="HP App Catalog for TouchPad">Restored App Catalog (webOS 3.0.x)</a><br>
         <a class="download-link" href="AppPackages/com.palm.app.findapps_3.0.23300_all.ipk"><img src="assets/palm-appcatalog.png" style="vertical-align:middle;height:48px;width:48px;" alt="HP App Catalog" title="HP App Catalog for Phones">Restored App Catalog (webOS 2.2.4)</a><br>
         <a class="download-link" href="https://github.com/h8pewou/legacy-webos-feeds/blob/main/README.md#wosa-feed"><img src="assets/preware-icon.png" style="vertical-align:middle;height:48px;width:48px;" alt="Preware Feed for Old Phones" title="Preware Feed for Older Phones">Preware Feed (webOS 1.x - LuneOS)</a><br>
